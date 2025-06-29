1cc473a250a290e4cfa0e156fa719a7114616f90
Josetamara12 <jose.tamara426@pascualbravo.edu.co>
2025-06-29 02:49:36 -0500

    Se ajusta el logo y algunos comentarios del AuthModals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 // src/app/page.tsx
 import Navbar from "@/components/ui/Navbar";
-//import Hero from "@/sections/Hero";
-//import Footer from "@/sections/Footer";
+import Hero from "@/sections/Hero";
+import Footer from "@/sections/Footer";
 
 export default function HomePage() {
   return (
     <main>
       
       <Navbar />
-      {/* <Hero />  
-      <Footer /> */}
+       <Hero />  
+      <Footer />
       
     </main>
   );
